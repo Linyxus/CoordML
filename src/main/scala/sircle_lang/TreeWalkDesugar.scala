56200a4ce0edfbbc7d8be43c46e6631eec2b59aa
Yichen Xu <yichen.x@outlook.com>
2020-11-23 11:01:34 +0800

    Update TreeWalkDesugar to desugar for

--- src/main/scala/sircle_lang/TreeWalkDesugar.scala
+++ src/main/scala/sircle_lang/TreeWalkDesugar.scala
@@ -11,6 +11,13 @@ trait TreeWalkDesugar {
     case ExprTuple(items) => desugar(ExprTuple(items map transform))
     case ExprIf(cond, left, right) => desugar(ExprIf(transform(cond), transform(left), right map transform))
     case ExprMapping(pairs) => desugar(ExprMapping(pairs map { x => x._1 -> transform(x._2) }))
+    case ExprFor(combinators, expr) => desugar(ExprFor(
+      combinators.map {
+        case ForBind(name, expr) => ForBind(name, transform(expr))
+        case ForFilter(expr) => ForFilter(transform(expr))
+      },
+      transform(expr)
+    ))
     case x => desugar(x)
   }
 }
