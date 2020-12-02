ea61e35883c474bacd1918a0d9de3f06843b90ea
Yichen Xu <yichen.x@outlook.com>
2020-12-02 17:25:48 +0800

    Add logging for diagnosing Sircle resolver

--- src/main/scala/central/ExpBlueprint.scala
+++ src/main/scala/central/ExpBlueprint.scala
@@ -14,8 +14,12 @@ case class ExpBlueprint(title: String,
 object ExpBlueprint {
   def fromRequest(request: CreateExpRequest): Either[String, ExpBlueprint] =
     try {
+      println(s"Creating experiment from request $request")
       val resolver = sircle_lang.Runtime.compileFunc(List(request.resolverPath), "resolve")
+      println(s"Obtained resolver function $resolver")
+      println(s"Parsing args from ${request.config}")
       val arg = sircle_lang.Value.fromJson(request.config)
+      println(s"Parsed args $arg")
       val task = resolver(List(arg)) match {
         case ValTask(task) => task
         case v => throw RuntimeError(s"Returned value type of resolve is ${v.valueType}, expecting task.")
@@ -24,6 +28,8 @@ object ExpBlueprint {
     } catch {
       case RuntimeError(msg) => Left("Runtime error: " + msg)
       case ParseError(msg) => Left("Parse error: " + msg)
-      case _ => Left("Unknown error.")
+      case e =>
+        e.printStackTrace()
+        Left("Unknown error.")
     }
 }
